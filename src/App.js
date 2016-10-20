058cc020ec2ba29957f4292797b6b705ef85d604
M.Charattrakool <m.charattrakool@gmail.com>
2016-10-20 12:54:08 +0700

    add GifList,GifItem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
-import SearchBar from './component/SearchBar';
+import SearchBar from './components/SearchBar';
+import GifList from './components/GifList';
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            gifs: [
+                {
+                    id: 1,
+                    url: 'http://fakeimg.pl/300/'
+                },
+                {
+                    id: 2,
+                    url: 'http://fakeimg.pl/300/'
+                },
+                {
+                    id: 3,
+                    url: 'http://fakeimg.pl/300/'
+                }
+            ]
+        }
+    }
 
     handleTermChange(term) {
         console.log(term);
@@ -11,6 +33,7 @@ class App extends React.Component {
         return (
             <div className="greeting">
                 <SearchBar onTermChange={this.handleTermChange} />
+                <GifList gifs={this.state.gifs} />
             </div>
         );
     }
